f522f7aecec02cc4852c1adccc8783b67f44de19
aysllanferreira <aysllan.ferreiraks@gmail.com>
2023-05-20 14:24:55 -0300

    :sparkles: feat: Hero finished

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import '../../styles/Hero/Hero.scss';
+import Pokeball from '../../assets/pokeball.svg';
 
 export default class Hero extends Component {
   render() {
     return (
-      <div>Hero</div>
+      <div className="Header">
+        <img className="Header__img" src={Pokeball} alt="Pokeball" />
+        <h1 className="Header__title">Pokedex React</h1>
+        <img className="Header__img" src={Pokeball} alt="Pokeball2" />
+      </div>
     );
   }
 }
